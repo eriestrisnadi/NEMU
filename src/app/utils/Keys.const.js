93790888d8ea5f8913690b0f229f53f4bbf2f732
Eries Trisnadi <me@eries.id>
2018-02-07 03:26:30 +0700

    separate keys to const file, use database to get the keyboards settings

---
+++ src/app/utils/Keys.const.js
@@ -0,0 +1,86 @@
+import { Controller } from 'jsnes';
+
+const KEYS = [
+  {
+    controller: 1,
+    key: Controller.BUTTON_A,
+    value: 88
+  },
+  {
+    controller: 1,
+    key: Controller.BUTTON_B,
+    value: 90
+  },
+  {
+    controller: 1,
+    key: Controller.BUTTON_SELECT,
+    value: 17
+  },
+  {
+    controller: 1,
+    key: Controller.BUTTON_START,
+    value: 13
+  },
+  {
+    controller: 1,
+    key: Controller.BUTTON_UP,
+    value: 38
+  },
+  {
+    controller: 1,
+    key: Controller.BUTTON_DOWN,
+    value: 40
+  },
+  {
+    controller: 1,
+    key: Controller.BUTTON_LEFT,
+    value: 37
+  },
+  {
+    controller: 1,
+    key: Controller.BUTTON_RIGHT,
+    value: 39
+  },
+  {
+    controller: 2,
+    key: Controller.BUTTON_A,
+    value: 103
+  },
+  {
+    controller: 2,
+    key: Controller.BUTTON_B,
+    value: 105
+  },
+  {
+    controller: 2,
+    key: Controller.BUTTON_SELECT,
+    value: 99
+  },
+  {
+    controller: 2,
+    key: Controller.BUTTON_START,
+    value: 97
+  },
+  {
+    controller: 2,
+    key: Controller.BUTTON_UP,
+    value: 104
+  },
+  {
+    controller: 2,
+    key: Controller.BUTTON_DOWN,
+    value: 98
+  },
+  {
+    controller: 2,
+    key: Controller.BUTTON_LEFT,
+    value: 100
+  },
+  {
+    controller: 2,
+    key: Controller.BUTTON_RIGHT,
+    value: 102
+  }
+];
+
+export default KEYS;
